8f22842b5e65cd8df0bd96983d1e9d4cfcddbb6d
gabrielfranca42 <gabrielfranca172@gmail.com>
2025-09-02 10:02:16 -0300

    primeiro commit criando ambiente spring

---
+++ src/test/java/pratica/aprendizado/spring/pratica/aprendiazdo/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pratica.aprendizado.spring.pratica.aprendiazdo.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
