1fe180e429d85899a6c6c479cf1a2e56e9bbe22e
GABRIEL EDUARDO DE MOURA FRANCA <gabriel.franca0020015928@edu.pe.senac.br>
2025-09-03 14:56:34 -0300

    adicionando o model

--- src/main/java/Model/Model.java
+++ src/main/java/Model/Model.java
@@ -1,4 +1,39 @@
 package Model;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "model")
 public class Model {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO )
+    private Long id;
+
+    private String nome;
+    private Double preco;
+
+    public Model() {}
+
+    public Model(String nome, Double preco){
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
 }
