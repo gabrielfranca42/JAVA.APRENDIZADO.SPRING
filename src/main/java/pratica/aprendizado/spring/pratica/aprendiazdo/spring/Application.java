91f664d509b010e1b286a8c70b8ed9f244ec53de
GABRIEL EDUARDO DE MOURA FRANCA <gabriel.franca0020015928@edu.pe.senac.br>
2025-09-03 14:09:41 -0300

    testando se o pc do senac comita direito

--- src/main/java/pratica/aprendizado/spring/pratica/aprendiazdo/spring/Application.java
+++ src/main/java/pratica/aprendizado/spring/pratica/aprendiazdo/spring/Application.java
@@ -17,6 +17,8 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
+	System.out.println("hello world se essa budega commita");
+    }
+
 
 }
