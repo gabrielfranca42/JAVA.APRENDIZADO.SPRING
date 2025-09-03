c8f9f69d8b55c8f4d47aed4abf7ae8e7d2104364
GABRIEL EDUARDO DE MOURA FRANCA <gabriel.franca0020015928@edu.pe.senac.br>
2025-09-03 15:47:48 -0300

    adição do service, adição do repository e model

---
+++ src/main/java/Repository/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package Repository;
+
+
+import Model.Model;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository <Model, Long> {
+
+
+}
+
