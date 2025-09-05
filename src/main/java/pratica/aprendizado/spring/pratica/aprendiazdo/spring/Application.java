30840e1badc15f626511939dde8dcec0ef65ec94
gabrielfranca42 <gabrielfranca172@gmail.com>
2025-09-05 20:02:59 -0300

    apenas adiçao de comentarios no codigo

--- src/main/java/pratica/aprendizado/spring/pratica/aprendiazdo/spring/Application.java
+++ src/main/java/pratica/aprendizado/spring/pratica/aprendiazdo/spring/Application.java
@@ -14,6 +14,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 //
 
 @SpringBootApplication
+//ao ser feito o model que vc quer que seja feito deploy use esse exemplo de baixo fazendo a devida referencia a entidade
 @EntityScan("model")
 public class  Application {
 
