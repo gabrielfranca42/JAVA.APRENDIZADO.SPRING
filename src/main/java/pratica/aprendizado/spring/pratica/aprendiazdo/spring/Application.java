33598bace63f3a1d015abe19be8910efa896d459
GABRIEL EDUARDO DE MOURA FRANCA <gabriel.franca0020015928@edu.pe.senac.br>
2025-09-08 13:52:23 -0300

    teste na faculdade

--- src/main/java/pratica/aprendizado/spring/pratica/aprendiazdo/spring/Application.java
+++ src/main/java/pratica/aprendizado/spring/pratica/aprendiazdo/spring/Application.java
@@ -20,7 +20,7 @@ public class  Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	System.out.println("hello world se essa budega commita");
+	System.out.println("hello world se essa budega commita xdxdxdxd");
     }
 
 
