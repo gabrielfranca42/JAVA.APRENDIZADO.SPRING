57216af49434bde98073691a94f8bb135078759d
GABRIEL EDUARDO DE MOURA FRANCA <gabriel.franca0020015928@edu.pe.senac.br>
2025-09-10 15:10:02 -0300

    adicao e criacao de completa do JwtAuthFilter no security

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -11,7 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-public class Controller {
+public class
+Controller {
 
    private final Service service;
 
