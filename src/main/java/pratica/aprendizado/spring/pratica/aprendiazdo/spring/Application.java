099d668163c7791daa3b2cdb36142318ef453264
GABRIEL EDUARDO DE MOURA FRANCA <gabriel.franca0020015928@edu.pe.senac.br>
2025-09-10 13:33:25 -0300

    teste pra ver se o git da faculdade ta pegando apenas nao muda nada do pus anterior

--- src/main/java/pratica/aprendizado/spring/pratica/aprendiazdo/spring/Application.java
+++ src/main/java/pratica/aprendizado/spring/pratica/aprendiazdo/spring/Application.java
@@ -21,7 +21,7 @@ public class  Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	System.out.println("hello world se essa budega commita xdxdxdxd");
+	System.out.println("apenas modificando para testar o git na faculdade");
     }
 
 
