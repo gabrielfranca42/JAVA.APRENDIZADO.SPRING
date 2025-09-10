c468f5c6573d4b627dba8430271aa0e805703e7f
gabrielfranca42 <gabrielfranca172@gmail.com>
2025-09-10 11:28:25 -0300

    eu sou um animal e nao tinha adicionado os arquivos mas continua igual ao commit anterior

--- src/main/java/Controller/AuthController.java
+++ src/main/java/Controller/AuthController.java
@@ -1,4 +1,41 @@
 package Controller;
 
+import Model.Usuarios;
+import Service.UsuarioService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import security.JwtUtil;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    private final UsuarioService usuarioService;
+
+
+    public AuthController (UsuarioService usuarioService){
+        this.usuarioService = usuarioService;
+    }
+//isso esta conectado a registro criptografado via Jwt
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody Map<String, String> request){
+        Usuarios usuarios = usuarioService.registrarUsuario(request.get("username"), request.get("password"));
+        return ResponseEntity.ok(usuarios);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Map<String, String> request) {
+        Optional<Usuarios> usuarios = usuarioService.buscarPorUsername(request.get("username"));
+        if (usuarios.isPresent() && usuarios.get().getPassword().equals(request.get("password"))) {
+            String token = JwtUtil.generateToken(usuarios.get().getUsername());
+            return ResponseEntity.ok(Map.of("token", token));
+        }
+        return ResponseEntity.status(401).body("Credencias invalidas");
+    }
 }
--- src/main/java/security/JwtUtil.java
+++ src/main/java/security/JwtUtil.java
@@ -6,11 +6,9 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
 import java.security.Key;
-import java.security.KeyStore;
-import java.security.Signature;
 import java.util.Date;
 
-public class JwUtil {
+public class JwtUtil {
 
     private static final Key  key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
     private static  final long EXPIRATION_TIME = 86400000;
