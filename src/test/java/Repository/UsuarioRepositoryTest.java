7406101d89f497e761b36bfa8688653f0e5fc947
gabrielfranca42 <gabrielfranca172@gmail.com>
2025-09-18 11:19:28 -0300

    criaco do teste unitario de usuarios UsuarioRepository

--- src/test/java/Repository/UsuarioRepositoryTest.java
+++ src/test/java/Repository/UsuarioRepositoryTest.java
@@ -1,15 +1,53 @@
 package Repository;
 
+import Model.Usuarios;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import pratica.aprendizado.spring.pratica.aprendiazdo.spring.Application;
 
+import javax.swing.text.html.parser.Entity;
+
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 @DataJpaTest
+@ActiveProfiles("test")
+@ContextConfiguration(classes = Application.class)
 class UsuarioRepositoryTest {
 
-    @Test
-    void findUserByUsername(){
 
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+
+    @Autowired
+    EntityManager entityManager;
+
+
+    @Test
+    @DisplayName("Should get User successfully from DB")
+    void findUserByUsernameCase1(){
+        Usuarios usuarios = new Usuarios("fernado","xdxd");
+    }
+
+
+    private Usuarios createUsername(Usuarios username){
+        Usuarios usuarios = new Usuarios();
+        this.entityManager.persist(usuarios);
+        this.entityManager.flush();
+
+        Optional<Usuarios> result = usuarioRepository.findByUsername("fernado");
+
+        assertTrue(result.isPresent());
+        assertEquals("fernado", result.get().getUsername());
+
+        return usuarios;
     }
 
 }
