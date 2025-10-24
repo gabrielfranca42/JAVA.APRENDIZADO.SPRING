69e27c31f7f6c1d295b6008b928b33da8a3e31d5
gabrielfranca42 <gabrielfranca172@gmail.com>
2025-10-24 10:54:06 -0300

    fudeo

--- src/main/java/security/JwtUtil.java
+++ src/main/java/security/JwtUtil.java
@@ -9,7 +9,7 @@ import java.security.Key;
 import java.util.Date;
 
 public class JwtUtil {
-
+//fudeo
     private static final Key  key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
     private static  final long EXPIRATION_TIME = 86400000;
 
